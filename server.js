77c1369e73779a750d69b3582982af6f8b7fd047
nklucas <Zhiyu@Zhiyus-MacBook-Pro.local>
2016-03-26 21:35:17 -0700

    server.js changed a little bit

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 
+
+
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+//We can do something similar for the MongoDB connection string:
+
+//// default to a 'localhost' configuration:
+//var connection_string = 'mongodb://127.0.0.1:27017/cs5610spring2016';
+//
+//// if OPENSHIFT env variables are present, use the available connection info:
+//if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+//    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//        process.env.OPENSHIFT_APP_NAME;
+//}
+//
+//var db = mongoose.connect(connection_string);
+
+
 
 app.listen(port, ipaddress);
